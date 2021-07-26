a45411ce5a1d4f037958cf016c8c911b70741cf5
MehrshadPeyman <mshd.p.22@gmail.com>
2021-07-26 18:04:16 +0430

    frontend UI done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import AddTask from './components/AddTask';
 
 
 function App() {
-
+  const [showAddTask, setShowAddTask] = useState(false);
    const [tasks, setTasks] = useState(
  [
     {
@@ -54,8 +54,9 @@ function App() {
 
   return (
     <div className="container">
-      <Header title='Task Tracker' />
-      <AddTask onAdd={ addTask }/>
+      <Header onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={ addTask }/>}
       {tasks.length > 0 ? <Tasks tasks={tasks}
         onDelete={deleteTask}
         onToggle={toggleReminder} /> : 'No Task to show'}
--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -22,32 +22,36 @@ const AddTask = ({onAdd }) => {
     }
 
     return (
-        <div className='add-form' onSubmit={onSubmit}>
+        <form className='add-form' onSubmit={onSubmit}>
             <div className="form-control">
                 <label>Task</label>
-                <input type="text" placeholder='Add Task'
-                    value={text} onChange={(e) =>
-                        setText(e.target.value)} />
+                <input
+                    type="text"
+                    placeholder='Add Task'
+                    value={text}
+                    onChange={(e) => setText(e.target.value)} />
             </div>
+
             <div className="form-control">
                 <label>Day & Time</label>
-                <input type="text" value={day}
-                    onChange={(e) =>
-                        setDay(e.target.value)}
-                    placeholder='Add Day & Time' />
+                <input
+                    type="text"
+                    placeholder='Add Day & Time' 
+                    value={day}
+                    onChange={(e) =>setDay(e.target.value)}/>
             </div>
+
             <div className="form-control form-control-check">
                 <label>Set Reminder</label>
                 <input type="checkbox"
                     checked={reminder}
                     value={reminder}
-                    onChange={(e) =>
-                        setReminder(e.currentTarget.checked)} />
+                    onChange={(e) =>setReminder(e.currentTarget.checked)} />
             </div>
 
             <input type="submit" value='Save Task'
                 className='btn btn-block' />
-        </div>
+        </form>
     )
 }
 
